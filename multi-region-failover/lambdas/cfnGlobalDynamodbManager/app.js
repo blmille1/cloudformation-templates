18a8668b0c8bae7d4305d99634dd87f506caf663
Brandon Miller <bmiller@bloombergindustry.com>
2021-02-24 21:23:54 -0600

    Fix Tags and Update events...again

--- multi-region-failover/lambdas/cfnGlobalDynamodbManager/app.js
+++ multi-region-failover/lambdas/cfnGlobalDynamodbManager/app.js
@@ -2,7 +2,6 @@
 const GlobalDynamodbManager = require('./global-dynamodb-manager');
 
 exports.handler = async (event, context) => {
-    console.log(JSON.stringify(event), JSON.stringify(context));
     let manager = new GlobalDynamodbManager();
     await manager.processEvent(event, context);
 };
